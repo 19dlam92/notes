035384a7d2ed96d8507439c438383b33e4927e4c
19dlam92 <19dlam92@gmail.com>
2023-02-28 11:54:43 -0500

    incorporating templates

--- flask/server.py
+++ flask/server.py
@@ -1,11 +1,67 @@
-from flask import flask
+from flask import Flask, render_template
 app = Flask(__name__)
 # Recognized after FLASK is installed
+# Flask
+    # installed package for using Flask
+# render_template
+    # required import to connect HTML and ROUTING
+    # NEEDS to have HTML in the same name
+        # refer to @app.route('/httpsresponse') return statement
+# redirect
+
+# session
+
+# request
 
 
 
+@app.route('/')
+def hello_world():
+    return 'Hello World'
 
 
+
+@app.route('/hello/<name>')
+# PARAMETER with < > needs to be CASTED below
+def hello(name):
+# PARAMETER within function matches CASTED variable from ROUTE
+    return f'Hello { name }'
+
+
+
+@app.route('/<int:number>')
+# PARAMETER with < > needs to be CASTED below
+def number(number):
+# PARAMETER within function matches CASTED variable from ROUTE
+    newNum = number * 5
+    return f'The number is { newNum }'
+
+
+
+@app.route('/display')
+def display():
+    return render_template('display.html')
+    # This renders your basic HTML template
+
+
+
+@app.route('/adv')
+def adv():
+    student_info = [
+        { 'name' : 'Will', 'age' : 35},
+        { 'name' : 'John', 'age' : 30},
+        { 'name' : 'Mark', 'age' : 25},
+        { 'name' : 'KB', 'age' : 27}
+    ]
+    return render_template('adv.html', students = student_info)
+    # students = student_info
+        # displays info from the backend
+        # pass this into the RENDER_TEMPLATE
+    # student_info ( purple )
+        # comes from the BACKEND
+    # students ( green )
+        # comes from the set HTML
+        # connects to the for loop
 
 
 
